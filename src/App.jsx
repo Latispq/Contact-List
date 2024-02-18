05a24034290bab4ecab7a7d6e0d91b5dfe90b758
Latispq <Latispq@gmail.com>
2024-02-18 12:22:18 -0500

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
-import './App.css'
-
-function App() {
-
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-
+      {selectedContactId ? (
+        <SelectedContact
+          selectedContactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
+        />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
-  )
+  );
 }
-
-export default App
